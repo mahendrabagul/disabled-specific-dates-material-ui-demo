e4e7267e6cbd5654860484dc86d002257b403d8a
Mahendra Hiraman <--edit>
2018-02-01 15:34:37 +1100

    Added a filter for date

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,15 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
-  myDates: Date[] = [new Date('Feb 06 2018 00:00:00 GMT+1100 '), new Date('Feb 05 2018 00:00:00 GMT+1100 ')];
+  myDates: string[] = [new Date('Feb 06 2018 00:00:00 GMT+1100').toUTCString(), new Date('Feb 05 2018 00:00:00 GMT+1100').toUTCString()];
   myFilter = (d: Date): boolean => {
-    for (var i = 0; i <= this.myDates.length; i++) {
-      console.log(this.myDates.length);
-      console.log('Array Value=' + this.myDates[i].toLocaleDateString());
-      console.log('Current Value=' + d.toLocaleDateString());
-      console.log('Result=' + (d.toLocaleDateString() === this.myDates[i].toLocaleDateString()));
-      return d.toLocaleDateString() === this.myDates[i].toLocaleDateString();
-    }
+    return this.myDates.includes(d.toUTCString());
   }
   constructor() {
   }
